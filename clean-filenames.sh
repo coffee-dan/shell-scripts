38af0da90f8140c68217a79532c7865888e217d6
emmanuel <emmanuelrocha00@gmail.com>
2019-03-07 18:37:14 -0600

    prints out changes

--- clean-filenames.sh
+++ clean-filenames.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo Translating .JPG and the like to lowercase \(i.e. .jpg\)
-rename 's/\.[^.]$/.\L$1/gi' *
+rename -v 's/\.[^.]$/.\L$1/gi' *
 echo Remove single quotes and commas
-rename "s/[',!]//g" *
+rename -v "s/[',!]//g" *
 echo Substituting \& with and
-rename 's/\&/and/g' *
+rename -v 's/\&/and/g' *
 echo Substituting blank spaces with underscores
-rename 's/\s/_/g' *
+rename -v 's/\s/_/g' *
 echo Substituting left over underscores within _-_ or _--_
-rename 's/_\-_/\-/g' *
-rename 's/_\-\-_/\-\-/g' *
+rename -v 's/_\-_/\-/g' *
+rename -v 's/_\-\-_/\-\-/g' *
