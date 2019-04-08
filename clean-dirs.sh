aa7e775295200852b1d6beb49798c74cfd78b42d
ubuntu-dan <coffee-dan@protonmail.com>
2019-04-08 15:32:44 -0500

    Organization and small edits

--- clean-dirs.sh
+++ clean-dirs.sh
@@ -1,17 +1,36 @@
 #!/bin/bash
 echo Retrieving directories matching pattern of album directory
 echo ...
+
 echo Translating .JPG and the like to lowercase \(i.e. .jpg\)
-rename 's/\.[^.]$/.\L$1/gi' *
-echo Removing punctuation \(\' , \!\)
-rename "s/[',!]//g" *
+# rename 's/\.[^.]$/.\L$1/gi' *
+find . -maxdepth 1 -type d -print0 \
+  | xargs -0 -I {} rename 's/\.[^.]$/.\L$1/g' "{}"
+
+echo Removing punctuation \(\' , \! \?\)
+# rename "s/[',!?]//g" *
+find . -maxdepth 1 -type d -print0 \
+  | xargs -0 -I {} rename "s/[',!?]//g" "{}"
+
 echo Substituting \& with and
-rename 's/\&/and/g' *
+# rename 's/\&/and/g' *
+find . -maxdepth 1 -type d -print0 \
+  | xargs -0 -I {} rename 's/\&/and/g' "{}"
+
 echo Substituting open and closed parens with \( --\> __ and \) removed
-rename 's/\(/__/g' *
-rename 's/\)//g' *
+# rename 's/\(/__/g' *
+find . -maxdepth 1 -type d -print0 \
+  | xargs -0 -I {} rename 's/\(/__/g' "{}"
+# rename 's/\)//g' *
+find . -maxdepth 1 -type d -print0 \
+  | xargs -0 -I {} rename 's/\)//g' "{}"
+
 echo Substituting blank spaces with underscores
-rename 's/\s/_/g' *
+# rename 's/\s/_/g' *
+find . -maxdepth 1 -type d -print0 \
+  | xargs -0 -I {} rename 's/\s/_/g' "{}"
 echo Substituting left over underscores within _-_ or _--_
-rename 's/_\-_/\-/g' *
-rename 's/_\-\-_/\-\-/g' *
+find . -maxdepth 1 -type d -print0 \
+  | xargs -0 -I {} rename 's/_\-_/\-/g' "{}"
+find . -maxdepth 1 -type d -print0 \
+  | xargs -0 -I {} rename 's/_\-\-_/\-\-/g' "{}"
--- clean-filenames.sh
+++ clean-filenames.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 echo Translating .JPG and the like to lowercase \(i.e. .jpg\)
-rename -v 's/\.[^.]$/.\L$1/gi' *
+rename -v 's/\.[^.]$/.\L$1/g' *
 echo Remove single quotes and commas
-rename -v "s/[',!]//g" *
+rename -v "s/[',!?]//g" *
 echo Substituting \& with and
 rename -v 's/\&/and/g' *
 echo Substituting blank spaces with underscores
 rename -v 's/\s/_/g' *
+# Implement parens logic
 echo Substituting left over underscores within _-_ or _--_
 rename -v 's/_\-_/\-/g' *
 rename -v 's/_\-\-_/\-\-/g' *
